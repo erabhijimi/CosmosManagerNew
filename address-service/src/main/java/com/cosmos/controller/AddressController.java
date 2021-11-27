816a44a2f47458f717d3986cba3824b7aa16e0cc
Abhijit <erabhijimi@gmail.com>
2021-11-27 15:27:20 +0530

    Updated address for UI sync

--- address-service/src/main/java/com/cosmos/controller/AddressController.java
+++ address-service/src/main/java/com/cosmos/controller/AddressController.java
@@ -3,11 +3,13 @@ package com.cosmos.controller;
 import com.cosmos.model.Address;
 import com.cosmos.model.Addresses;
 import com.cosmos.service.AddressService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/address")
+@Slf4j
 public class AddressController {
     @Autowired
     private AddressService addressService;
@@ -17,14 +19,17 @@ public class AddressController {
     }
     @GetMapping("/{mobileNumber}")
     public Address getAddressForUser(@PathVariable Long mobileNumber){
+        log.info("getAddressForUser called for"+mobileNumber);
         return addressService.getAddressForUser(mobileNumber);
     }
     @PostMapping
     public Address saveAddress(@RequestBody Address address){
+        log.info("saveAddress called for"+address.toString());
         return addressService.saveAddress(address);
     }
     @PutMapping("/{mobileNumber}")
     public Address updateAddressForUser(@PathVariable Long mobileNumber,@RequestBody Address address){
+        log.info("updateAddressForUser called for"+address.toString()+" "+mobileNumber);
         return addressService.updateAddressForUser(mobileNumber,address);
     }
 }
--- address-service/src/main/java/com/cosmos/model/Address.java
+++ address-service/src/main/java/com/cosmos/model/Address.java
@@ -3,19 +3,21 @@ package com.cosmos.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Data
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "address003")
 public class Address {
     @Id
-    private Long custId;
+    private Long mobileNumber;
     private String custName;
     private String custAddressLine1;
     private String landmark;
